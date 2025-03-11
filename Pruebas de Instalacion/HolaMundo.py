6003379624d724d0ae18bcbd6bd9662fd4a478f3
Carlos Juárez <cjuarez@uca.edu.sv>
2025-03-10 18:28:12 -0600

    Primeros códigos

---
+++ Pruebas de Instalacion/HolaMundo.py
@@ -0,0 +1,6 @@
+''' Los tres apóstrofes al inicio y al final de un texto permiten escribir comentarios de varias líneas.
+    Este ejemplo muestra cómo imprimir un mensaje en pantalla. '''
+# El númeral al inicio de una línea indica un comentario de una sola línea.
+# print es la función que imprime un mensaje en pantalla.
+print("Hola Mundo")
+print('Tu programa se ha ejecutado correctamente')
