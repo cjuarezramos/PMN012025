5016e8c584c2e897376e2fe9ac636677a73423b9
Carlos Juárez <cjuarez@uca.edu.sv>
2026-03-16 11:42:50 -0600

    archivo de clase seccion 01 pmn 2026

---
+++ PMN012026_S01/Primerprograma.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 11:22:01 2026
+
+@author: carlosjuarez
+Descripción: Primer programa de PMN
+"""
+
+print('Hola mundo!')
+print("Bienvenidos a PMN")
+print('Seccion ')
+print(1)
+
