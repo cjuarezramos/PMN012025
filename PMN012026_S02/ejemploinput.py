1db075e2603f110e4aa05dd64d1be50903fe6fe2
Carlos Juárez <cjuarez@uca.edu.sv>
2026-03-17 12:37:59 -0600

    ejercicio input

---
+++ PMN012026_S02/ejemploinput.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 12:26:11 2026
+
+@author: carlosjuarez
+descripcion del input
+"""
+
+r = input('Ingrese el radio: ')
+r = float(r)
+pi = 3.1416
+A = pi*r**2
+P = 2*pi*r
+
+# f para cambiar las variables
+# \n para salto de linea
+print(f'El área es: {A}\nEl perímetro es: {P}')
+
+
---
+++ PMN012026_S02/tiposdevariables.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 12:02:38 2026
+
+@author: carlosjuarez
+descripcion tipos de datos
+"""
+nombre = "Carlos"
+edad = 43
+estatura = 1.75
+es_docente = True
+
+
+# Conjunto de datos
+# Listas
+# Conjunto de datos y se pueden modificar
+# usa corchetes y separado por coma
+lista1 = [nombre,edad,estatura,es_docente]
+print(lista1)
+lista1[1] = 42
+print(lista1)
+
+#Tuplas
+#Conjunto de datos no permutables
+tupla1 = (nombre,edad,estatura,es_docente)
+
+# Diccionarios
+# Estructuras de datos, definidos por claves
+diccionario1 = {
+    'Nombre': nombre,
+    'Edad': edad,
+    'Estatura': estatura,
+    'Profesor': es_docente,
+    'Profesor': False}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
