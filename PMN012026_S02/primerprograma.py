88c664c419cfbec91ab041de4605b8551915af1b
Carlos Juárez <cjuarez@uca.edu.sv>
2026-03-17 11:36:25 -0600

    primer ejemplo seccion 01

---
+++ PMN012026_S02/primerprograma.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 11:14:46 2026
+
+@author: carlosjuarez
+Descripcion primer programa
+
+"""
+print('Hola mundo')
+print("esta año es")
+print(2026)
