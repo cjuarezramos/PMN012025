52d3dcda7bb79626a91e3e20283807c899c133db
Carlos Juárez <cjuarez@uca.edu.sv>
2026-03-17 11:56:07 -0600

    archivo de variables

---
+++ PMN012026_S02/programadevariable.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 11:35:29 2026
+
+@author: carlosjuarez
+
+descripciòn: Variables
+"""
+r = 16 # almacenar 16 en la variable r
+pi = 3.1416 # almaceno pi en memoria
+
+A = pi*r**2
+P = 2*pi*r
+
+print('El área es')
+print(A)
+
+print(f'El perimetro es: {P}')
+
