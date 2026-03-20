913c288fbd361f0ffbbb9a61575318bde1152fea
Carlos Juárez <cjuarez@uca.edu.sv>
2026-03-20 09:18:54 -0600

    actualizando repo

---
+++ PMN012026_S02/OperacionesRelacionales.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 11:26:59 2026
+
+@author: carlosjuarez
+Operaciones relacionales y lògicas
+"""
+
+print(f'4>3 = {4>3} ')
+print(f'4<3 = {4<3} ')
+print(f'4>=3 = {4>=3} ')
+print(f'4>4 = {4>4} ')
+print(f'4>=4.0 = {4>=4.0} ')
+print(f'4==3 = {4==3} ')
+print(f'4!=3 = {4!=3} ')
+print(f'4==4.0 = {4==4.0} ')
+
+
---
+++ PMN012026_S02/Operacionestexto.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 11:23:20 2026
+
+@author: carlosjuarez
+
+Operaciones texto
+"""
+a = input('Ingrese un texto: ')
+b = input('Ingrese otro texto: ')
+
+print(a+b)
+print(a*3)
